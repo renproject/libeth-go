b6d7754bde5ce717583e6415f7cb41496f521efa
susruth <susruth@republicprotocol.com>
2019-05-09 10:26:27 +1000

    add ERC20 view

--- erc20.go
+++ erc20.go
@@ -10,16 +10,21 @@ import (
 )
 
 type erc20 struct {
+	client  *Client
 	account *account
 	cerc20  *CompatibleERC20
 }
 
 type ERC20 interface {
-	BalanceOf(ctx context.Context, who common.Address) (*big.Int, error)
-	Allowance(ctx context.Context, owner, spender common.Address) (*big.Int, error)
+	ERC20View
 	Transfer(ctx context.Context, to common.Address, amount *big.Int, speed TxExecutionSpeed, sendAll bool) (*types.Transaction, error)
 	Approve(ctx context.Context, spender common.Address, amount *big.Int, speed TxExecutionSpeed) (*types.Transaction, error)
 	TransferFrom(ctx context.Context, from, to common.Address, amount *big.Int, speed TxExecutionSpeed) (*types.Transaction, error)
+}
+
+type ERC20View interface {
+	BalanceOf(ctx context.Context, who common.Address) (*big.Int, error)
+	Allowance(ctx context.Context, owner, spender common.Address) (*big.Int, error)
 }
 
 func (account *account) NewERC20(addressOrAlias string) (ERC20, error) {
@@ -31,16 +36,32 @@ func (account *account) NewERC20(addressOrAlias string) (ERC20, error) {
 	if err != nil {
 		return nil, err
 	}
+	client := account.Client()
 	return &erc20{
+		client:  &client,
 		account: account,
 		cerc20:  compatibleERC20,
 	}, nil
 }
 
+func (client *Client) NewERC20View(addressOrAlias string) (ERC20View, error) {
+	address, ok := client.addrBook[addressOrAlias]
+	if !ok {
+		address = common.HexToAddress(addressOrAlias)
+	}
+	compatibleERC20, err := NewCompatibleERC20(address, bind.ContractBackend(client.EthClient()))
+	if err != nil {
+		return nil, err
+	}
+	return &erc20{
+		client: client,
+		cerc20: compatibleERC20,
+	}, nil
+}
+
 func (erc20 *erc20) BalanceOf(ctx context.Context, who common.Address) (*big.Int, error) {
-	client := erc20.account.Client()
 	var balance *big.Int
-	return balance, client.Get(ctx, func() error {
+	return balance, erc20.client.Get(ctx, func() error {
 		bal, err := erc20.cerc20.BalanceOf(&bind.CallOpts{}, who)
 		if err != nil {
 			return err
@@ -51,9 +72,8 @@ func (erc20 *erc20) BalanceOf(ctx context.Context, who common.Address) (*big.Int
 }
 
 func (erc20 *erc20) Allowance(ctx context.Context, owner, spender common.Address) (*big.Int, error) {
-	client := erc20.account.Client()
 	var allowance *big.Int
-	return allowance, client.Get(ctx, func() error {
+	return allowance, erc20.client.Get(ctx, func() error {
 		alw, err := erc20.cerc20.Allowance(&bind.CallOpts{}, owner, spender)
 		if err != nil {
 			return err
